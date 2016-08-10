e4df3291bd8d0b696923932e853ed6376f345913
asmisloff <a.smisloff@gmail.com>
2016-08-10 22:07:49 +0300

    1

--- ht-2/ex-1.php
+++ ht-2/ex-1.php
@@ -1,10 +1,20 @@
+<meta charset="utf-8">
 <?php
 function strings_to_paragraphs($ar, $flag) {
     $strings = "";
     foreach ($ar as $s) {
         $strings = $strings . "<p> $s </p>";
     }
+    echo $strings;
     if ($flag) {
-        return $strings;
+        return implode($ar, "");
+    }
+    else {
+        return null;
     }
 }
+
+$test = ["Буря", "Мглою", "Небо", "кроет"];
+echo strings_to_paragraphs($test, false);
+echo strings_to_paragraphs($test, true);
+?>
