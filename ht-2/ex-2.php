6a26ce71d8bcb56f6ed272db8341ba1366575b3b
asmisloff <a.smisloff@gmail.com>
2016-08-12 22:04:55 +0300

    ---

--- ht-2/ex-2.php
+++ ht-2/ex-2.php
@@ -5,34 +5,35 @@ $operators = [
     "+" => function($x, $y) {return $x + $y;},
     "-" => function($x, $y) {return $x - $y;},
     "/" => function($x, $y) {return $x / $y;},
-    "*" => function($x, $y) {return $x * $y;}
+    "*" => function($x, $y) {return $x * $y;},
+    "**" => function($x, $y) {return pow($x, $y);},
 ];
-
-$arr = [1, 2, 3, 4, 5];
 
 function apply($op, $arg1, $arg2) {
     global $operators;
+    $err_msg = "APPLY -- неизвестный оператор -- '$op' <br>";
     if (array_key_exists($op, $operators)) {
         $proc = $operators[$op];
         return $proc($arg1, $arg2);
     } 
     else {
-        return NULL;
+        return $err_msg;
     } 
 }
 
 function reduce($arr, $op, $initial_value) {
+    $err_msg = "REDUCE -- не числовой элемент во входном массиве -- '$num' <br>";
     foreach ($arr as $num) {
-	$initial_value = apply($op, $initial_value, $num);
+        $initial_value = apply($op, $initial_value, $num);
+	if (is_string($initial_value)) {
+	    return $initial_value;
+	}
+	if (!is_numeric($num)) {
+            return $err_msg;
+        }
     }
     return $initial_value;
 }
 
-$test = [1, 2, 3, 4, 5, 6, 7];
-$acc = reduce($test, "/", 1);
-if ($acc) {
-    echo $acc;
-}
-else {
-    echo "В reduce передан неизвестный оператор";
-}
+$test = [1, 2, 3, 4, 5, 6, 8];
+echo $acc = reduce($test, "**", 0.99999);
